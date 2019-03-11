7fa63f0dd7987cbb8cf41fb3e6454e0fb39b9ed7
Will Jarvis <will@toohotlimited.com>
2019-03-11 17:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+ 
+ if (feet<=400) {
+   return "This one is on me!"
+ } else if (feet>2500) {
+   return "No can do."
+ } else {
+   return "I will gladly take your thirty bucks."
+ }
+ 
+}
+
+
+  
+function ternaryCheckCity(city) {
+  return city== 'NYC' ? "Ok, sounds good." : "No go."
+}
+
+
+
+
+function switchOnCharmFromTip(tip) {
+
+  let = tip
+  
+switch (tip) {
+  if (tip === 'generous') {
+    "Thank you so much.";
+    break;
+} else if (tip === 'not as generous') {
+    "Thank you."
+    break;
+} else (tip === 'thanks for everything') {
+    "Bye."
+}
+}
+
+  
+  
+  
